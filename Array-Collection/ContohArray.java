7895dd27b8e4d90f43255e8517da520d2166dc56
mayaTI2A <mayadprccc@gmail.com>
2025-10-01 15:20:48 +0700

    Menambahkan file Array di package Array-Collection

---
+++ Array-Collection/ContohArray.java
@@ -0,0 +1,11 @@
+public class ContohArray {
+    public static void main(String[] args) {
+        // Deklarasi dan inisialisasi array
+        String[] nama = {"Maya", "Dwi", "Putri", "Ramadhani"};
+
+        // Menampilkan seluruh elemen array
+        for (int i = 0; i < nama.length; i++) {
+            System.out.println("Index " + i + ": " + nama[i]);
+        }
+    }
+}
