268937b4578e4bcf32b4a1c8667ed9d2c565dee8
mayaTI2A <mayadprccc@gmail.com>
2025-10-01 15:04:56 +0700

    Menambahkan file mahasiswa view di package view

---
+++ view/MahasiswaView.java
@@ -0,0 +1,14 @@
+package view;
+
+import model.Mahasiswa;
+
+public class MahasiswaView {
+
+     public static void main(String[] args) {
+        
+        Mahasiswa mahasiswa = new Mahasiswa();
+        mahasiswa.ipk = 3.7f;
+
+    }
+    
+}
